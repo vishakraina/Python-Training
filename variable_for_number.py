431a1feec88503847f753751aed964fb8a203b55
vishakraina <78915709+vishakraina@users.noreply.github.com>
2024-10-28 23:45:19 +0530

    Add files via upload

---
+++ variable_for_number.py
@@ -0,0 +1,50 @@
+# Exercise 3 - Variable for numbers
+
+# Assigning numbers to a varibale/
+
+weight = 70
+
+weight + 25
+
+# Numbers vs Strings
+
+height = "170"  # height is a string
+age = 25        # age is a number
+
+# Using variable in calculations
+
+original_num = 23
+new_num = original_num + 7
+print(new_num)   #output: 30
+
+#Using Multiple Variables in a Calculation
+
+original_num = 23
+num_to_be_added = 7
+new_num = original_num + num_to_be_added
+print(new_num)           # Output: 30
+
+#Variables as Their Own New Value
+
+original_num = 90
+original_num = original_num + 10
+print(original_num)      # Output: 100
+
+#Variables Can Change Types
+
+your_age = "21"          # Now `your_age` is a string
+your_age = 21            # Now `your_age` is a number
+
+#Naming Variables with Numbers
+
+# Incorrect:
+#1st_prime_number = 2     # Starts with a number - not allowed
+
+# Correct:
+prime_number_1st = 2     # Number comes after the letters - allowed
+
+#Integer and Float Variables
+
+temperature = 37.5       # Float
+balance = -100.75        # Float
+percentage = 0.003       # Float
