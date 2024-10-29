a51e6e4b3f3cdbd3df25a6b2ea0357feb7f7feef
vishakraina <78915709+vishakraina@users.noreply.github.com>
2024-10-29 22:18:12 +0530

    Add files via upload

---
+++ Concatenating_Text_String.py
@@ -0,0 +1,29 @@
+# Topic 8 - Concatenating Text Strings
+
+# Example of Concatenation:
+
+greeting = "Hello"
+addressee = "World"
+whole_greeting = greeting + addressee
+print(whole_greeting)  # Output: HelloWorld
+
+
+# 1.	Concatenating Variables
+
+greeting = "Hello"
+addressee = "World"
+whole_greeting = greeting + ", " + addressee + "!"
+print(whole_greeting)  # Output: Hello, World!
+
+#  2.	Concatenating Directly
+
+print("Hello, " + "World!")  # Output: Hello, World!
+
+# 3.	Mixing Variables and Strings
+
+punc = "!"
+print("Hello" + ", " + "World" + punc)  # Output: Hello, World!
+
+# 4.	Concatenation Rules
+
+print("The sum of 2 plus 2 is " + "4")  # Output: The sum of 2 plus 2 is 4
